92c9c406ee0fa902316a8fd1b2daace34cc67099
melon362 <67920715+melon362@users.noreply.github.com>
2021-02-17 14:58:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var inputEl = document.getElementById("input");
+var textEl = document.getElementById("text");
 inputEl.addEventListener("onchange",function(){
-	
-})
+	textEl.innerText = inputEl.value;
+});
