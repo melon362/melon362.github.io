46f0df401a4f669f349a330fab58c3822a0d86f4
melon362 <>
2021-02-25 19:32:07 -0500

    Initial commit

--- projects/spacebar/script.js
+++ projects/spacebar/script.js
@@ -1,6 +1,7 @@
 var space = Number(localStorage.getItem('space'));
 var save = document.getElementById("save")
 var text = document.getElementById("text")
+var reset = document.getElementById("reset")
 if (space == null){
 	localStorage.setItem('space',0)
 	var space = 0
@@ -15,4 +16,9 @@ window.addEventListener("keyup", function(e){
 });
 save.addEventListener("click", function(){
 	localStorage.setItem('space',space)
+});
+reset.addEventListener("click", function(){
+	space = 0
+        localStorage.setItem('space',0)
+	text.innerText = "You have hit spacebar 0 times";
 });
